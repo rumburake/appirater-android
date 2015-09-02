86102165d2a35c7607a94e9f04268691fe68804b
RumburaK <rumburake@gmail.com>
2015-09-02 01:01:09 +0100

    added support for proper Dialog lifecycle management using DialogFragment, removed maven crap

--- src/main/java/com/sbstrm/appirater/Appirater.java
+++ src/main/java/com/sbstrm/appirater/Appirater.java
@@ -1,19 +1,5 @@
 package com.sbstrm.appirater;
 
-import android.annotation.SuppressLint;
-import android.app.Dialog;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.Uri;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.Window;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
 /*	
  * @source https://github.com/sbstrm/appirater-android
  * @license MIT/X11
@@ -27,7 +13,22 @@ import android.widget.TextView;
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
+
 public class Appirater {
+
+    public enum RateDialogAction {
+        RATE_YES,
+        RATE_LATER,
+        RATE_NO
+    }
 
     private static final String PREF_LAUNCH_COUNT = "launch_count";
     private static final String PREF_EVENT_COUNT = "event_count";
@@ -37,51 +38,69 @@ public class Appirater {
     private static final String PREF_DATE_FIRST_LAUNCHED = "date_firstlaunch";
     private static final String PREF_APP_VERSION_CODE = "versioncode";
 
-    public static void appLaunched(Context mContext, RatingButtonListener ratingButtonListener) {
+    private static Appirater sAppirater;
+
+    public static Appirater getRater(Context context) {
+        if (sAppirater == null) {
+            sAppirater = new Appirater(context);
+        }
+        return sAppirater;
+    }
+
+    Appirater(Context context) {
+        mContext = context;
+        mPrefs = context.getSharedPreferences(context.getPackageName() + ".appirater", 0);
+        mEditor = mPrefs.edit();
+    }
+
+    Context mContext;
+    SharedPreferences mPrefs;
+    SharedPreferences.Editor mEditor;
+
+    public void appLaunched(FragmentActivity activity) {
         boolean testMode = mContext.getResources().getBoolean(R.bool.appirator_test_mode);
-        SharedPreferences prefs = mContext.getSharedPreferences(mContext.getPackageName() + ".appirater", 0);
-        if (!testMode && (prefs.getBoolean(PREF_DONT_SHOW, false) || prefs.getBoolean(PREF_RATE_CLICKED, false))) {
+        if (!testMode && (mPrefs.getBoolean(PREF_DONT_SHOW, false) || mPrefs.getBoolean(PREF_RATE_CLICKED, false))) {
             return;
         }
 
-        SharedPreferences.Editor editor = prefs.edit();
+        mEditor = mPrefs.edit();
 
         if (testMode) {
-            showRateDialog(mContext, editor, ratingButtonListener);
+            showRateDialog(activity);
             return;
         }
 
         // Increment launch counter
-        long launch_count = prefs.getLong(PREF_LAUNCH_COUNT, 0);
+        long launch_count = mPrefs.getLong(PREF_LAUNCH_COUNT, 0);
 
         // Get events counter
-        long event_count = prefs.getLong(PREF_EVENT_COUNT, 0);
+        long event_count = mPrefs.getLong(PREF_EVENT_COUNT, 0);
 
         // Get date of first launch
-        long date_firstLaunch = prefs.getLong(PREF_DATE_FIRST_LAUNCHED, 0);
+        long date_firstLaunch = mPrefs.getLong(PREF_DATE_FIRST_LAUNCHED, 0);
 
         // Get reminder date pressed
-        long date_reminder_pressed = prefs.getLong(PREF_DATE_REMINDER_PRESSED, 0);
+        long date_reminder_pressed = mPrefs.getLong(PREF_DATE_REMINDER_PRESSED, 0);
 
         try {
             int appVersionCode = mContext.getPackageManager().getPackageInfo(mContext.getPackageName(), 0).versionCode;
-            if (prefs.getInt(PREF_APP_VERSION_CODE, 0) != appVersionCode) {
+            if (mPrefs.getInt(PREF_APP_VERSION_CODE, 0) != appVersionCode) {
                 //Reset the launch and event counters to help assure users are rating based on the latest version.
                 launch_count = 0;
                 event_count = 0;
-                editor.putLong(PREF_EVENT_COUNT, event_count);
+                mEditor.putLong(PREF_EVENT_COUNT, event_count);
             }
-            editor.putInt(PREF_APP_VERSION_CODE, appVersionCode);
+            mEditor.putInt(PREF_APP_VERSION_CODE, appVersionCode);
         } catch (Exception e) {
             //do nothing
         }
 
         launch_count++;
-        editor.putLong(PREF_LAUNCH_COUNT, launch_count);
+        mEditor.putLong(PREF_LAUNCH_COUNT, launch_count);
 
         if (date_firstLaunch == 0) {
             date_firstLaunch = System.currentTimeMillis();
-            editor.putLong(PREF_DATE_FIRST_LAUNCHED, date_firstLaunch);
+            mEditor.putLong(PREF_DATE_FIRST_LAUNCHED, date_firstLaunch);
         }
 
         // Wait at least N days and/or M events before opening
@@ -95,128 +114,65 @@ public class Appirater {
                 int timedEvents = mContext.getResources().getInteger(R.integer.appirator_timed_events_until_prompt);
                 if ((timedEvents == 0) || (event_count >= timedEvents)) {
                     if (date_reminder_pressed == 0) {
-                        showRateDialog(mContext, editor, ratingButtonListener);
+                        showRateDialog(activity);
                     } else {
                         long remindMillisecondsToWait = mContext.getResources().getInteger(R.integer.appirator_days_before_reminding) * 24 * 60 * 60 * 1000L;
                         if (System.currentTimeMillis() >= (remindMillisecondsToWait + date_reminder_pressed)) {
-                            showRateDialog(mContext, editor, ratingButtonListener);
+                            showRateDialog(activity);
                         }
                     }
                 }
             }
         }
 
-        editor.apply();
+        mEditor.commit();
     }
 
-    @SuppressLint("CommitPrefEdits")
-    public static void rateApp(Context mContext) {
-        SharedPreferences prefs = mContext.getSharedPreferences(mContext.getPackageName() + ".appirater", 0);
-        SharedPreferences.Editor editor = prefs.edit();
-        rateApp(mContext, editor);
-    }
-
-    public static void significantEvent(Context mContext) {
+    public void significantEvent() {
         boolean testMode = mContext.getResources().getBoolean(R.bool.appirator_test_mode);
         SharedPreferences prefs = mContext.getSharedPreferences(mContext.getPackageName() + ".appirater", 0);
         if (!testMode && (prefs.getBoolean(PREF_DONT_SHOW, false) || prefs.getBoolean(PREF_RATE_CLICKED, false))) {
             return;
         }
-
         long event_count = prefs.getLong(PREF_EVENT_COUNT, 0);
         event_count++;
-        prefs.edit().putLong(PREF_EVENT_COUNT, event_count).apply();
+        prefs.edit().putLong(PREF_EVENT_COUNT, event_count).commit();
     }
 
-    private static void rateApp(Context mContext, final SharedPreferences.Editor editor) {
-        mContext.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(String.format(mContext.getString(R.string.appirator_market_url), mContext.getPackageName()))));
-        if (editor != null) {
-            editor.putBoolean(PREF_RATE_CLICKED, true);
-            editor.commit();
-        }
-    }
-
-    @SuppressLint("NewApi")
-    private static void showRateDialog(final Context mContext, final SharedPreferences.Editor editor,
-                                       final RatingButtonListener ratingButtonListener) {
+    private void showRateDialog(FragmentActivity activity) {
+        RateDialog rateDialog = new RateDialog();
+        Bundle args = new Bundle();
         String appName = mContext.getString(R.string.appirator_app_title);
-        final Dialog dialog = new Dialog(mContext);
-
-        // no title because we can't control the color...title is now in the layout
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-        @SuppressLint("InflateParams")
-        LinearLayout layout = (LinearLayout) LayoutInflater.from(mContext).inflate(R.layout.appirater, null);
-
-        TextView title = (TextView) layout.findViewById(R.id.title);
-        title.setText(String.format(mContext.getString(R.string.appirator_rate_title), appName));
-
-        TextView tv = (TextView) layout.findViewById(R.id.message);
-        tv.setText(String.format(mContext.getString(R.string.appirator_message), appName));
-
-        Button rateButton = (Button) layout.findViewById(R.id.rate);
-        rateButton.setText(String.format(mContext.getString(R.string.appirator_button_rate), appName));
-        rateButton.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                rateApp(mContext, editor);
-                dialog.dismiss();
-                if (ratingButtonListener != null) {
-                    ratingButtonListener.onRate();
-                }
-            }
-        });
-
-        Button rateLaterButton = (Button) layout.findViewById(R.id.rateLater);
-        rateLaterButton.setText(mContext.getString(R.string.appirator_button_rate_later));
-        rateLaterButton.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                if (editor != null) {
-                    editor.putLong(PREF_DATE_REMINDER_PRESSED, System.currentTimeMillis());
-                    editor.commit();
-                }
-                dialog.dismiss();
-                if (ratingButtonListener != null) {
-                    ratingButtonListener.onLater();
-                }
-            }
-        });
-
-        Button cancelButton = (Button) layout.findViewById(R.id.cancel);
-        cancelButton.setText(mContext.getString(R.string.appirator_button_rate_cancel));
-        cancelButton.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                if (editor != null) {
-                    editor.putBoolean(PREF_DONT_SHOW, true);
-                    editor.commit();
-                }
-                dialog.dismiss();
-                if (ratingButtonListener != null) {
-                    ratingButtonListener.onDeclined();
-                }
-            }
-        });
-
-        dialog.setContentView(layout);
-        dialog.show();
+        args.putString("title", String.format(mContext.getString(R.string.appirator_rate_title), appName));
+        args.putString("message", String.format(mContext.getString(R.string.appirator_message), appName));
+        args.putString("yes", String.format(mContext.getString(R.string.appirator_button_rate), appName));
+        args.putString("later", mContext.getString(R.string.appirator_button_rate_later));
+        args.putString("no", mContext.getString(R.string.appirator_button_rate_cancel));
+        rateDialog.setArguments(args);
+        rateDialog.show(activity.getSupportFragmentManager(), "appirater");
     }
 
-    /**
-     * Listener for finding out whether a button has been pressed by the user
-     */
-    public static abstract class RatingButtonListener {
-        /**
-         * Called if the user clicks on the Rate button to go to the Play store
-         */
-        public abstract void onRate();
-
-        /**
-         * Called if the user clicks on the Later button to remind them later
-         */
-        public abstract void onLater();
-
-        /**
-         * Called if the user clicks on the No/Cancel button so they're no longer prompted
-         */
-        public abstract void onDeclined();
+    public void rateAction(RateDialogAction rateDialogAction) {
+        switch (rateDialogAction) {
+            case RATE_YES:
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(String.format(mContext.getString(R.string.appirator_market_url), mContext.getPackageName())));
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                try {
+                    mContext.startActivity(i);
+                } catch (ActivityNotFoundException e) {
+                    // no market
+                }
+                mEditor.putBoolean(PREF_RATE_CLICKED, true);
+                mEditor.commit();
+                break;
+            case RATE_LATER:
+                mEditor.putLong(Appirater.PREF_DATE_REMINDER_PRESSED, System.currentTimeMillis());
+                mEditor.commit();
+                break;
+            case RATE_NO:
+                mEditor.putBoolean(Appirater.PREF_DONT_SHOW, true);
+                mEditor.commit();
+                break;
+        }
     }
 }
